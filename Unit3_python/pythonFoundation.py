b846c04c93801411913c7ffc84e4975a97647f11
muhammed green <mgreen339@boyslatin.net>
2024-10-31 13:32:50 +0000

    Learning python with mr. Ian

---
+++ Unit3_python/pythonFoundation.py
@@ -0,0 +1,42 @@
+# data type = basic building block of python programming.
+
+# string = data represented as chacters wrapped 
+# together in quotation marks
+"this is a string"
+
+# interger = whole numbers.
+678
+
+# floats = decimal numbers
+687.99
+
+# boolean = true or false values
+True
+False
+
+
+# variables = contaniners for data
+# we create variables using the following steps
+# 1. create a variable name
+# 2. apply an equal sign
+# 3. give it a data type value
+
+user= "Draco"
+message ='Good morning,'
+weather =" It is 60 degress and sunny"
+
+# we use print to see data in our terminal
+print(user + message + ". "+ weather)
+
+number1= 1200
+number2=800
+
+print(number1, number2)
+
+name='my name is muhammed'
+activity ='I am going to go swimming'
+time='I will do this at 2:00pm'
+print(name + activity+ time)
+
+
+
