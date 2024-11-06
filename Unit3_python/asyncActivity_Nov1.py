91ab11dfa3566e2c3ae7b983e3f8a3d527a00285
muhammed green <mgreen339@boyslatin.net>
2024-11-06 14:23:40 +0000

    i worked on my async activity and i am almost done.

---
+++ Unit3_python/asyncActivity_Nov1.py
@@ -0,0 +1,7 @@
+number1=200
+number2=45
+print(number1+number2)
+step2="to create a calender you strings for the days of the week and integers for the dates"
+step3= "a floats are decimal numbers, while intergers are whole numbers"
+step4= "The correct date type for a commet section would be string. The correct data type for a survey would be boolean."
+print(step2,step3,step4)
